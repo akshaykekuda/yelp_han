Fix auc calculation and save loss plot

Inference_fns.py
@@ -21,13 +21,13 @@ def get_metrics(dataloader, encoder):
     target_arr =[]
     raw_pred_arr = []
     encoder.eval()
-    e
     with torch.no_grad():
         for batch in tqdm(dataloader):
             output, scores = encoder(batch['indices'], batch['lens'], batch['review_pos_indices'], batch['word_pos_indices'])
             probs = F.softmax(output, dim=1)
             max_vals = torch.max(probs, dim=1)
-            raw_proba = max_vals[0].tolist()
+            # raw_proba = max_vals[0].tolist()
+            raw_proba = probs[:, 1].cpu()
             pred = max_vals[1].tolist()
             # pred = torch.argmax(output, dim=1).tolist()
             target = batch['category'].tolist()
@@ -38,7 +38,7 @@ def get_metrics(dataloader, encoder):
             if scores is None:
                 attn_score_arr.extend([None for i in range(len(pred))])
             else:
-                attn_score_arr.extend(scores.numpy())
+                attn_score_arr.extend(scores.cpu().numpy())
         raw_pred_df = pd.DataFrame(raw_pred_arr, columns=['RawPred Proba'])
         pred_df = pd.DataFrame(pred_arr, columns=['Pred Category'])
         target_df = pd.DataFrame(target_arr, columns=['True Category'])
@@ -66,7 +66,7 @@ def plot_roc(df, path):
     auc = metrics.roc_auc_score(y_true, y_pred_proba).round(2)
     plt.plot(fpr, tpr, label="auc={}".format(auc))
     plt.xlabel("False Positivity Rate")
-    plt.xlabel("True Positivity Rate")
+    plt.ylabel("True Positivity Rate")
     plt.legend(loc=4)
     plt.show()
     plt.savefig(path)

MainYelp.py
@@ -11,6 +11,7 @@ import argparse
 
 from DatasetClasses import YelpDataset
 from torch.utils.data import DataLoader
+from gensim.models import KeyedVectors
 from gensim.models.keyedvectors import Word2VecKeyedVectors
 import numpy as np
 import torch
@@ -21,7 +22,7 @@ from Inference_fns import get_metrics, plot_roc
 np.random.seed(0)
 torch.manual_seed(0)
 
-word_embedding_pt = dict(glove='../word_embeddings/glove.w2v.txt',
+word_embedding_pt = dict(glove='../word_embeddings/glove_word_vectors',
                          w2v='..word_embeddings/custom_w2v_100d',
                          fasttext='../word_embeddings/fasttext_300d.bin')
 
@@ -92,14 +93,14 @@ def run_yelp_model():
     dataset_train.save_vocab('vocab')
     c = Collate(vocab, args.device)
     dataloader_transcripts_train = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=True,
-                                              num_workers=4, collate_fn=c.yelp_collate)
+                                              num_workers=0, collate_fn=c.yelp_collate)
     dataloader_transcripts_dev = DataLoader(dataset_dev, batch_size=args.batch_size, shuffle=False,
-                                            num_workers=4, collate_fn=c.yelp_collate)
+                                            num_workers=0, collate_fn=c.yelp_collate)
     dataloader_transcripts_test = DataLoader(dataset_test, batch_size=args.batch_size, shuffle=False,
-                                             num_workers=4, collate_fn=c.yelp_collate)
+                                             num_workers=0, collate_fn=c.yelp_collate)
 
     print("Start loading glove vectors")
-    model = Word2VecKeyedVectors.load_word2vec_format(word_embedding_pt[args.word_embedding])
+    model = KeyedVectors.load(word_embedding_pt[args.word_embedding], mmap='r')
     print("Finished loading glove vectors")
     vec_size = model.vector_size
     vocab_size = len(vocab)